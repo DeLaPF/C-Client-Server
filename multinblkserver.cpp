626e848be98e7df2d2396fec976773d99835760f
De La Petrillo-Foster <delapf@localhost.localdomain>
2019-12-29 16:14:04 -0500

    added comments

--- multinblkserver.cpp
+++ multinblkserver.cpp
@@ -9,10 +9,14 @@
 #include "ThreadPool.h"
 #include "fdlist.h"
 
-void handleConnnection(int sockfd, fd_set *read_fds_ref, struct node *head);
+const int PORT_NO = 8080;
+const int BACKLOG_SIZE = 2;
+
+// Prototypes
+void select(int sockfd, fd_set *read_fds_ref, struct node *head);
 bool wasEchoed(fd_set *read_fds_ref, struct node *head);
-void echo(fd_set *read_fds_ref, struct node *head, int fd);
-void accept(struct node *head, int sockfd);
+void echo(int fd);
+int accept(int sockfd);
 
 void error(const char *msg)
 {
@@ -23,32 +27,29 @@ void error(const char *msg)
 int main(int argc, char const *argv[])
 {
 	// Step 1: Set up socket
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0); // Create socket file descriptor for server
     if (sockfd < 0) 
         error("ERROR opening socket");
-    int portno = 8080;
 
 	// Step 2: Bind
 	struct sockaddr_in host;
-	host.sin_family = AF_INET;
-	host.sin_port = htons(portno);
-	host.sin_addr.s_addr = INADDR_ANY;
+	host.sin_family = AF_INET; // IPv4 connection
+	host.sin_port = htons(PORT_NO); // bound to port 8080
+	host.sin_addr.s_addr = INADDR_ANY; // connected to localhost
 	const struct sockaddr *host_ptr = (struct sockaddr*) &host;
-	int bound = bind(sockfd, host_ptr, sizeof(host));
+	int bound = bind(sockfd, host_ptr, sizeof(host)); // bind sockfd to port
 	if (bound < 0)
 		error("ERROR binding to socket");
 
 	// Step 3: Listen
-	int listening = listen(sockfd, 2);
+	int listening = listen(sockfd, BACKLOG_SIZE); // The socket will actively listen for incoming connections and queue them up
 	if (listening < 0)
 		error("ERROR listener failed");
 
 	fd_set read_fds;
-	FD_SET(sockfd, &read_fds);
-	struct node *head = createFDList(0);
-
+	struct node *head = createFDList(0); // Create empty fdlist (file descriptor list)
 	while (1) {
-		handleConnnection(sockfd, &read_fds, head);
+		select(sockfd, &read_fds, head); // select incoming connections and handle them as necessary
 	}
 
 	// Close the connection
@@ -57,67 +58,72 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
-void handleConnnection(int sockfd, fd_set *read_fds_ref, struct node *head) {
+//Pre: sockfd is the socket bound to this server and head is a pointer to the beginning of an fdlist
+//Post: sleeps until there is an available connection and then determines if it must be accepted and read from
+void select(int sockfd, fd_set *read_fds_ref, struct node *head) {
 	printf("%s\n", "Attempting to Select . . .");
-	makeFDS(head, read_fds_ref);
-	FD_SET(sockfd, read_fds_ref);
-	int maxsockfd = maxfd(sockfd, head);
-	int selected = select(maxsockfd, read_fds_ref, NULL, NULL, NULL);
+	makeFDS(head, read_fds_ref); // make fd_set from fdlist
+	FD_SET(sockfd, read_fds_ref); // (re)add sockfd to allow for new connections
+	int maxsockfd = maxfd(sockfd, head); // determine the maximum value of file descriptor for select
+	int selected = select(maxsockfd, read_fds_ref, NULL, NULL, NULL); // sleep until there has been in a change in one of the file descriptors in the fd_set
 	if (selected >= 0) {
 		printf("%s\n", "Connection Selected");
-		bool wasechoed = 1;
 
-		if (!wasEchoed(read_fds_ref, head))
-			accept(head, sockfd);
+		if (!wasEchoed(read_fds_ref, head)) { // determine if the connection should be echoed
+			int remotefd = accept(sockfd); // accept connection to remote client through remotefd
+			pushToList(head, remotefd); // add remotefd to list
+		}
 	}
 	else {error("ERROR Selection failed");}
 }
 
-void accept(struct node *head, int sockfd) {
+//Pre: sockfd is the socket bound to this server
+//Post: accept connection and return the file descriptor of connected client
+int accept(int sockfd) {
 	// Step 4: Accecpt
-	struct sockaddr_in remote;
-	struct sockaddr *remote_ptr = (struct sockaddr*) &remote;
-	socklen_t remote_len = (socklen_t) sizeof(remote);
+	struct sockaddr *remote_host; // a pointer to a sockaddr struct that will be filled in with the address information of the remote host
+	socklen_t *remote_len; // the size of the sockaddr struct in bytes
 	printf("%s\n", "Attempting to Accept . . .");
-	int remotefd = accept(sockfd, remote_ptr, &remote_len);
+	int remotefd = accept(sockfd, remote_host, remote_len); // dequeue from sockfd and accept connection through remotefd
 	if (remotefd < 0)
 		error("ERROR receiving request");
-	if (remotefd != sockfd)
-		pushToList(head, remotefd);
 	printf("%s\n", "Connection Accecpted");
+	return remotefd;
 }
 
+//Pre: head is a pointer to the beginning of an fdlist
+//Post: return 1 if data was echoed and echoes it or return 0 if data does not need to be echoed
 bool wasEchoed(fd_set *read_fds_ref, struct node *head) {
 	struct node *cur_node = head;
-	while (cur_node != NULL) {
+	while (cur_node != NULL) { // iterate through fdlist
 		int fd = cur_node->data;
-		if (FD_ISSET(fd, read_fds_ref)) {
+		if (FD_ISSET(fd, read_fds_ref)) { // check if file descriptor is ready to be read from
 			printf("%s\n", "Echoing Data");
+			removeFromList(head, fd); // close connection to client part 1
+			FD_CLR(fd, read_fds_ref); // close connection to client part 2
 
-			ThreadPool thread_pool(4);
-			std::vector<std::future<void>> handles;
-			handles.emplace_back(thread_pool.enqueue([read_fds_ref, head, fd]{
-				echo(read_fds_ref, head, fd);
-	    	}));
-
-			return 1;
+			ThreadPool thread_pool(4); // create threadpool of size 4
+			thread_pool.enqueue([fd]{ // run "echo" asynchronously to improve not force clients to wait for another clients poosibly slow processes 
+				echo(fd); // echo  data from fd
+			});
+			return 1; // true
 		}
 		cur_node = cur_node->next;
 	}
-	return 0;
+	return 0; // false
 }
 
-void echo(fd_set *read_fds_ref, struct node *head, int fd) {
+//Pre: fd is a valid file descritor of a client that has sent data to the server
+//Post: return exact data back to client
+void echo(int fd) {
 	// Step 5: Echo
-	char buffer[256];
-	bzero(buffer, 256);
-	int recvd = recv(fd, buffer, 255, 0);
+	char buffer[256]; // create buffer of 256 chars
+	bzero(buffer, 256); // zero out all values in buffer
+	int recvd = recv(fd, buffer, 255, 0); // receive data from fd and put it in buffer
 	if (recvd < 0)
 		error("ERROR data not received");
-	int sent = send(fd, buffer, strlen(buffer), 0);
+	int sent = send(fd, buffer, strlen(buffer), 0); // send buffer back to echo data
 	if (sent < 0)
 		error("ERROR data not sent");
-	close(fd);
-	removeFromList(head, fd);
-	FD_CLR(fd, read_fds_ref);
+	close(fd); // close connection to client part 3
 }
