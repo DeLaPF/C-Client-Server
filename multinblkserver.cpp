011b404e721b14cbb81ed6c9537c2f2deaab8c22
De La Petrillo-Foster <delapf@localhost.localdomain>
2019-12-28 23:26:44 -0500

    moved fdlist and functions to new file

--- multinblkserver.cpp
+++ multinblkserver.cpp
@@ -7,6 +7,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include "ThreadPool.h"
+#include "fdlist.h"
 
 void handleConnnection(int sockfd, fd_set *read_fds_ref, struct node *head);
 bool wasEchoed(fd_set *read_fds_ref, struct node *head);
@@ -15,71 +16,6 @@ void error(const char *msg)
 {
     perror(msg);
     exit(0);
-}
-
-struct node {
-	int data;
-	struct node *next;
-};
-
-struct node* createList(int data) {
-	int size_of_node = sizeof(struct node);
-	struct node *head = (struct node*) malloc(size_of_node);
-	head->data = data;
-	head->next = NULL;
-	return head;
-}
-
-void pushToList(struct node *head_ref, int data) {
-	int size_of_node = sizeof(struct node);
-	struct node *new_node = (struct node*) malloc(size_of_node);
-	new_node->data = data;
-	struct node *temp_node = head_ref->next;
-	head_ref->next = new_node;
-	new_node->next = temp_node;
-	//printf("%i %s\n", data, "added");
-}
-
-int removeFromList(struct node *head_ref, int data) {
-	struct node *pre_node = head_ref;
-	struct node *cur_node = head_ref->next;
-	while(cur_node != NULL && cur_node->data != data) {
-		pre_node = cur_node;
-		cur_node = cur_node->next;
-	}
-	if (cur_node == NULL) {
-		return -1;
-	}
-
-	pre_node->next = cur_node->next;
-	cur_node->next = NULL;
-	free(cur_node);
-	//printf("%i %s\n", data, "removed");
-	return data;
-}
-
-void makeFDS(struct node *head_ref, fd_set *fds_ref) {
-	fd_set new_fds;
-	*fds_ref = new_fds;
-	struct node *cur_node = head_ref->next;
-	while(cur_node != NULL) {
-		int data = cur_node->data;
-		FD_SET(data, fds_ref);
-		cur_node = cur_node->next;
-	}
-}
-
-int maxfd(int sockfd, struct node *head_ref) {
-	int max = sockfd;
-	struct node *cur_node = head_ref->next;
-	while(cur_node != NULL) {
-		int data = cur_node->data;
-		if (data > max)
-			max = data;
-		cur_node = cur_node->next;
-	}
-
-	return max + 1;
 }
 
 int main(int argc, char const *argv[])
@@ -107,7 +43,7 @@ int main(int argc, char const *argv[])
 
 	fd_set read_fds;
 	FD_SET(sockfd, &read_fds);
-	struct node *head = createList(0);
+	struct node *head = createFDList(0);
 
 	while (1) {
 		handleConnnection(sockfd, &read_fds, head);
@@ -131,8 +67,7 @@ void handleConnnection(int sockfd, fd_set *read_fds_ref, struct node *head) {
 
 		ThreadPool thread_pool(4);
 		std::vector<std::future<void>> handles;
-		handles.emplace_back(
-			thread_pool.enqueue([read_fds_ref, head, &wasechoed]{
+		handles.emplace_back(thread_pool.enqueue([read_fds_ref, head, &wasechoed]{
 				wasechoed = wasEchoed(read_fds_ref, head);
     	}));
 
